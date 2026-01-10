b1d3b32b40545dea1fb6eab81a7ca8f370dc2d1a
Emil_30 <emilien.rey1@gmail.com>
2026-01-10 14:33:58 +0100

    spawner a Q

---
+++ Assets/Scripts/ToiletPaperSpawner.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ToiletPaperSpawner : MonoBehaviour
+{
+    [Header("Prefab du rouleau")]
+    public GameObject toiletPaperPrefab;
+
+    [Header("Point de spawn")]
+    public Transform spawnPoint;
+
+    [Header("Cooldown (anti spam)")]
+    public float spawnCooldown = 0.3f;
+
+    private float lastSpawnTime = 0f;
+
+    void Awake()
+    {
+        UnityEngine.XR.Interaction.Toolkit.Interactables.XRBaseInteractable interactable = GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactables.XRBaseInteractable>();
+        interactable.selectEntered.AddListener(OnInteract);
+    }
+
+    void OnInteract(SelectEnterEventArgs args)
+    {
+        if (Time.time - lastSpawnTime < spawnCooldown)
+            return;
+
+        Instantiate(toiletPaperPrefab, spawnPoint.position, spawnPoint.rotation);
+        lastSpawnTime = Time.time;
+    }
+}
